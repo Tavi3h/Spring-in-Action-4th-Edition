2107def11b1123ba80b4e97354b0915930960f7d
Tavish <bit_zjm@hotmail.com>
2018-08-15 19:15:44 +0800

    update p1c03-advanced_wiring-02_conditional codes

---
+++ sia4e-P1_Core_Spring-C03_Advanced_wiring-02_conditional/src/main/java/com/habuma/restfun/MagicExistsCondition.java
@@ -0,0 +1,13 @@
+package com.habuma.restfun;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class MagicExistsCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        return context.getEnvironment().containsProperty("magic");
+    }
+}
