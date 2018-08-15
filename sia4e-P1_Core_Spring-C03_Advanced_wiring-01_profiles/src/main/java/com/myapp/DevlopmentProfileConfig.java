cc8c23f95dc52de84c03630cacd82ccb5610d8aa
Tavish <bit_zjm@hotmail.com>
2018-08-15 14:45:20 +0800

    update p1c03-01_profiles codes

---
+++ sia4e-P1_Core_Spring-C03_Advanced_wiring-01_profiles/src/main/java/com/myapp/DevlopmentProfileConfig.java
@@ -0,0 +1,22 @@
+package com.myapp;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("dev")
+public class DevlopmentProfileConfig {
+    @Bean(destroyMethod = "shutdown")
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+}
