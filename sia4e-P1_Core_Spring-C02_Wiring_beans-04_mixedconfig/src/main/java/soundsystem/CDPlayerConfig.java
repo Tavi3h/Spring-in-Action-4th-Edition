6bbcbcd3afd3e389cfafd2152e4d93b972b4465e
Tavish <bit_zjm@hotmail.com>
2018-08-16 15:30:01 +0800

    remove unnessary annotation @Autowired

--- sia4e-P1_Core_Spring-C02_Wiring_beans-04_mixedconfig/src/main/java/soundsystem/CDPlayerConfig.java
+++ sia4e-P1_Core_Spring-C02_Wiring_beans-04_mixedconfig/src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,15 +1,14 @@
 package soundsystem;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CDPlayerConfig {
-    
+
     @Bean
-    public CDPlayer cdPlayer(@Autowired @Qualifier("sgtPeppers") CompactDisc compactDisc) {
+    public CDPlayer cdPlayer(@Qualifier("sgtPeppers") CompactDisc compactDisc) {
         return new CDPlayer(compactDisc);
     }
 }
