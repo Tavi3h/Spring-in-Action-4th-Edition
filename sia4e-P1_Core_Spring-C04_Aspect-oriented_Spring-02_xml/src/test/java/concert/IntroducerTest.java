e8366b337f4d08a3550e7a876d541af4cfd0b8ce
Tavish <bit_zjm@hotmail.com>
2018-08-18 14:16:19 +0800

    update p1c04_o2_xml

---
+++ sia4e-P1_Core_Spring-C04_Aspect-oriented_Spring-02_xml/src/test/java/concert/IntroducerTest.java
@@ -0,0 +1,28 @@
+package concert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import concert.Encoreable;
+import concert.Performance;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:concert.xml")
+public class IntroducerTest {
+
+    @Autowired
+    private Performance performance;
+
+    @Test
+    public void testEncore() {
+
+        performance.perform();
+
+        Encoreable encoreable = (Encoreable) performance;
+
+        encoreable.performEncore();
+    }
+}
---
+++ sia4e-P1_Core_Spring-C04_Aspect-oriented_Spring-02_xml/src/test/java/soundsystem/TrackCounterTest.java
@@ -0,0 +1,41 @@
+package soundsystem;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:soundsystem.xml")
+public class TrackCounterTest {
+    
+    @Rule
+    public final SystemOutRule log = new SystemOutRule().enableLog();
+    
+    @Autowired
+    private CompactDisc cd;
+    
+    @Autowired
+    private TrackCounter counter;
+    
+    @Test
+    public void testTrackCounter() {
+        cd.playTrack(1);
+        cd.playTrack(2);
+        cd.playTrack(3);
+        cd.playTrack(3);
+        cd.playTrack(3);
+        cd.playTrack(3);
+        cd.playTrack(4);
+        assertEquals(0, counter.getPlayCount(0));
+        assertEquals(1, counter.getPlayCount(1));
+        assertEquals(1, counter.getPlayCount(2));
+        assertEquals(4, counter.getPlayCount(3));
+        assertEquals(1, counter.getPlayCount(4));
+    }
+}
