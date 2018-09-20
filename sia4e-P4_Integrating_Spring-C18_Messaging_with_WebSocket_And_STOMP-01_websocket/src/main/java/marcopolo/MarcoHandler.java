60f6665c5ac792aef53dcffb38aeb6286876e07b
Tavish <bit_zjm@hotmail.com>
2018-09-20 17:03:52 +0800

    update p4 c18 websocket codes

---
+++ sia4e-P4_Integrating_Spring-C18_Messaging_with_WebSocket_And_STOMP-01_websocket/src/main/java/marcopolo/MarcoHandler.java
@@ -0,0 +1,32 @@
+package marcopolo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+public class MarcoHandler extends AbstractWebSocketHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(MarcoHandler.class);
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        logger.info("Received message: " + message.getPayload());
+        TimeUnit.SECONDS.sleep(2);
+        session.sendMessage(new TextMessage("Polo!"));
+    }
+    
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        logger.info("Connection established");
+    }
+    
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        logger.info("Connection closed. status: " + status);
+    }
+}
---
+++ sia4e-P4_Integrating_Spring-C18_Messaging_with_WebSocket_And_STOMP-01_websocket/src/main/java/marcopolo/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package marcopolo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    // 将MarcoHandler映射到“/marco”
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(marcoHandler(), "/marco");
+    }
+
+    // 声明MarcoHandler bean
+    @Bean
+    public MarcoHandler marcoHandler() {
+        return new MarcoHandler();
+    }
+
+}
